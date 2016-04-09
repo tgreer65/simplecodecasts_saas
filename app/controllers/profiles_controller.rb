3b5ed98634e0b85e23204b702f05fb624bb39b3a
Tim Greer <prodomojo@gmail.com>
2016-04-09 00:01:44 +0000

    Added form partial, and edit action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -6,16 +6,21 @@ class ProfilesController < ApplicationController
   end
   
   def create
-  @user = User.find( params[:user_id] )
-  @profile = @user.build_profile(profile_params)
-  if @profile.save
-    flash[:success] = "Profile Updated"
-    redirect_to user_path( params[:user_id] )
+    @user = User.find( params[:user_id] )
+    @profile = @user.build_profile(profile_params)
+    if @profile.save
+      flash[:success] = "Profile Updated"
+      redirect_to user_path( params[:user_id] )
   else
      render action: :new
+    end
   end
-end
 
+  def edit
+    @user = User.find( params[:user_id] )
+    @profile = @user.profile
+  end
+  
   private
     def profile_params
       params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
